ebf236201e1568571b7a5ff6573ab12e030808af
Aldin <alhuric89@gmail.com>
2023-02-26 13:20:29 +0100

    refactoring code

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
@@ -29,4 +29,33 @@ document.addEventListener('keydown', function (event) {
 
 overlay.addEventListener('click', function () {
   hideMe();
-});
+});*/
+
+//Refactoring
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// Functions
+const hideModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const showModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const handleEscape = event => {
+  if (event.key === 'Escape') {
+    hideModal();
+  }
+};
+
+// Event listeners
+btnsOpenModal.forEach(btn => btn.addEventListener('click', showModal));
+btnCloseModal.addEventListener('click', hideModal);
+overlay.addEventListener('click', hideModal);
+document.addEventListener('keydown', handleEscape);
